344cd4f3fb490e9a95bbbdb5b6c7e8aba23e396a
JoseVanterpool <josevanterpool95@gmail.com>
2016-07-24 16:19:06 +0100

    working on topics dropdown

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -118,7 +118,7 @@ angular.module('controllers', ['firebase'])
 
 })
 
-.controller('CreateDebateCtrl', function($scope, $state, fbUser, $window) {
+.controller('CreateDebateCtrl', function($scope, $state, fbUser, $ionicPopover) {
   $scope.goBackHome = function () {
     $state.go('tab.home')
   }
@@ -137,6 +137,35 @@ angular.module('controllers', ['firebase'])
 
    $state.go('mainDebate', {debateid : debateIDArg})
   }
+
+  // ********************** here ******************************
+  
+  // .fromTemplate() method
+  var template = '<ion-popover-view><ion-header-bar> <h1 class="title">Choose A Topic</h1> </ion-header-bar> <ion-content> List of Topics </ion-content></ion-popover-view>';
+
+  $scope.popover = $ionicPopover.fromTemplate(template, {
+    scope: $scope
+  });
+
+
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+  // Execute action on hide popover
+  $scope.$on('popover.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove popover
+  $scope.$on('popover.removed', function() {
+    // Execute action
+  });
 })
 
 .controller('MainDebateCtrl', function($scope, $stateParams, debateServ, $window, fbUser, $state){
